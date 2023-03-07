3de4f20b8e9b1c6558c4cbadca95ca69e599e000
WISNU MUHAMMAD RAMADHAN <89554821+rama1277@users.noreply.github.com>
2023-03-07 21:31:44 +0700

    Update scan-sub.py

--- scan-sub.py
+++ scan-sub.py
@@ -1,34 +1,54 @@
+# import requests
+
+# def find_subdomain(domain, timeout=3):
+#     wordlist = [
+#         "admin",
+#         "api",
+#         "blog",
+#         "chat",
+#         "image",
+#         "login",
+#         "mail"
+#     ]
+
+#     for subdomain in wordlist:
+#         url = f"http://{subdomain}.{domain}"
+#         try:
+#             response = requests.get(url)
+#             status = response.status_code
+#             print(f"{url} - {status}")
+#         except requests.exceptions.RequestException:
+#             pass
+        
+#         url = f"https://{subdomain}.{domain}"
+#         try:
+#             response = requests.get(url)
+#             status = response.status_code
+#             print(f"{url} - {status}")
+#         except request.exceptions.RequestExceptions:
+#             pass
+
+# domain = "google.com"
+# timeout = 4
+
+# find_subdomain(domain, timeout)
+
+
 import requests
 
-def find_subdomain(domain, timeout=3):
-    wordlist = [
-        "admin",
-        "api",
-        "blog",
-        "chat",
-        "image",
-        "login",
-        "mail"
-    ]
+domain = input("Enter domain: ")
+file = open('wordlist.txt','r')
+content = file.read()
 
-    for subdomain in wordlist:
-        url = f"http://{subdomain}.{domain}"
-        try:
-            response = requests.get(url)
-            status = response.status_code
-            print(f"{url} - {status}")
-        except requests.exceptions.RequestException:
-            pass
-        
-        url = f"https://{subdomain}.{domain}"
-        try:
-            response = requests.get(url)
-            status = response.status_code
-            print(f"{url} - {status}")
-        except request.exceptions.RequestExceptions:
-            pass
+subdomains = content.splitlines()
 
-domain = "google.com"
-timeout = 4
-
-find_subdomain(domain, timeout)
+for subdomain in subdomains:
+	url1 = f"http://{subdomain}.{domain}"
+	url2 = f"https://{subdomain}.{domain}"
+	try:
+		requests.get(url1)
+		print(f"Discovered URL: {url1}")
+		requests.get(url2)
+		print(f"Discovered URL: {url2}")
+	except requests.ConnectionError:
+		pass
